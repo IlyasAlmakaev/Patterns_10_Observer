611e34dbadfa13c94071013ed2be423d312a81b9
Ильяс <i.almakaev@gmail.com>
2017-08-02 15:00:39 +0700

    Initial Commit

---
+++ Patterns_10_Observer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_10_Observer
+//
+//  Created by Ильяс on 02.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
